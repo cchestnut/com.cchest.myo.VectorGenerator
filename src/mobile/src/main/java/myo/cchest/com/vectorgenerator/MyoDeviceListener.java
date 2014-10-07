204272fa908f9e9b3b06b6893c74ecfa151d1c38
cchestnut <cchest01@villanova.edu>
2014-10-06 23:25:35 -0400

    beginning architecture of system

---
+++ src/mobile/src/main/java/myo/cchest/com/vectorgenerator/MyoDeviceListener.java
@@ -0,0 +1,8 @@
+package myo.cchest.com.vectorgenerator;
+import com.thalmic.myo.AbstractDeviceListener;
+/**
+ * Created by Chestnut on 10/6/2014.
+ */
+public class MyoDeviceListener extends AbstractDeviceListener {
+    
+}

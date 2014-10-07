731f9c9b314b5a8e8762c6b5604926948790c0d8
Chris Chestnut <cchest01@villanova.edu>
2014-10-06 22:39:28 -0400

    successfully imported myo library

---
+++ src/mobile/src/main/java/myo/cchest/com/vectorgenerator/Controller.java
@@ -0,0 +1,19 @@
+package myo.cchest.com.vectorgenerator;
+
+/**
+ * Created by Chestnut on 10/6/2014.
+ */
+public class Controller {
+    public Controller(){
+
+    }
+    //public static boolean re
+    //save pose to DB
+    //calculate range of vectors being recorded
+    //connect to myo
+    //start record Vectors
+    //stop recording Vectors
+    //retrieve myo data
+    //check which Vectors to care about
+
+}
